3f3db8f1cf90f1992ae6185fcc98b64b9f8b2b01
Vital Ozierski <v.ozierski@blak-it.com>
2019-02-17 16:41:37 +0300

    HT1.4 - decorator

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Article from './article'
-import useAccordion from '../custom-hooks/accordion'
+import accordion from '../decorators/accordion'
 
-export default function ArticleList({ articles }) {
-  const { openItemId, toggleOpenItem } = useAccordion()
+class ArticlesList extends Component {
+  render() {
+    const { articles, openItemId, toggleOpenItem } = this.props
 
-  const articleItems = articles.map((article) => (
-    <li key={article.id}>
-      <Article
-        article={article}
-        onBtnClick={toggleOpenItem(article.id)}
-        isOpen={article.id === openItemId}
-      />
-    </li>
-  ))
+    const articleItems = articles.map((article) => (
+      <li key={article.id}>
+        <Article
+          article={article}
+          onBtnClick={toggleOpenItem(article.id)}
+          isOpen={article.id === openItemId}
+        />
+      </li>
+    ))
 
-  return <ul>{articleItems}</ul>
+    return <ul>{articleItems}</ul>
+  }
 }
+
+export default accordion(ArticlesList)
 
 //import AccodrionComponent from './accordion-component'
 /*
