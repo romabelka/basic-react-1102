c66317bceb92c88439c61a7dc03b0d3f5c240653
Vital Ozierski <v.ozierski@blak-it.com>
2019-02-17 16:29:01 +0300

    HT1.3 complete

--- src/components/comments-list.js
+++ src/components/comments-list.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react'
 import Comment from './comment'
 
 class CommentsList extends Component {
+  state = {
+    isOpen: false
+  }
+
   render() {
+    return (
+      <div style={{ marginTop: '20px', marginLeft: '50px' }}>
+        <h2>Comments: </h2>
+        {this.renderComments()}
+      </div>
+    )
+  }
+
+  renderComments() {
     const { collection } = this.props
+    const { isOpen } = this.state
+
+    if (collection.length === 0) {
+      return <span>No comments</span>
+    }
 
     const commentItems = collection.map((comment) => (
       <li key={comment.id}>
@@ -12,11 +30,17 @@ class CommentsList extends Component {
     ))
 
     return (
-      <div style={{ marginTop: '20px' }}>
-        <h2>Comments: </h2>
-        <ul>{commentItems}</ul>
-      </div>
+      <>
+        <button onClick={this.onToggleBtnClick.bind(this)}>{isOpen ? 'Close' : 'Show'}</button>
+        {isOpen ? <ul>{commentItems}</ul> : null}
+      </>
     )
+  }
+
+  onToggleBtnClick() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
   }
 }
 
