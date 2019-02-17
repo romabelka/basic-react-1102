039cd951e5f0144cd9dae8ad476d6caecafb6ee6
axaxat@gmail.com <axaxat@gmail.com>
2019-02-17 19:44:52 +0300

    hw1 - task 1.1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Select from 'react-select'
 import ArticleList from './components/article-list'
 import UserForm from './components/user-form'
 import articles from './fixtures'
+import DayPicker, { DateUtils } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
 
 class App extends Component {
   state = {
-    selected: null
+    selected: null,
+    from: null,
+    to: null
   }
 
   handleChange = (selected) => this.setState({ selected })
@@ -15,17 +19,28 @@ class App extends Component {
     console.log('---', state)
   }
 
+  handleDayClick = (day) => {
+    const { from, to } = this.state
+    const range = DateUtils.addDayToRange(day, { from, to })
+    this.setState(range)
+  }
+
   render() {
     const options = articles.map((article) => ({
       label: article.title,
       value: article.id
     }))
 
+    const { from, to } = this.state
     return (
       <div>
         <h1>Article App</h1>
         <UserForm onStateChange={this.getUserState} />
         <Select options={options} value={this.state.selected} onChange={this.handleChange} />
+        <DayPicker selectedDays={[from, { from, to }]} onDayClick={this.handleDayClick} />
+        <p>
+          {from && from.toLocaleDateString()} - {to && to.toLocaleDateString()}{' '}
+        </p>
         <ArticleList articles={articles} />
       </div>
     )
