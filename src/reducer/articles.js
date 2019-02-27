731fe63106b5106b50aaaa73537bb182aebc9b6f
Alexander Gruzdkov <gruzdkow.a@ya.ru>
2019-02-27 19:47:19 +0300

    ht 4.1 is done

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,12 +1,23 @@
-import { normalizedArticles as defaultArticles } from '../fixtures'
+import { normalizedArticles } from '../fixtures'
 import { DELETE_ARTICLE } from '../constants'
+
+const defaultArticles = normalizedArticles.reduce(
+  (acc, article) => ({
+    ...acc,
+    [article.id]: article
+  }),
+  {}
+)
 
 export default (articlesState = defaultArticles, action) => {
   const { type, payload } = action
 
   switch (type) {
-    case DELETE_ARTICLE:
-      return articlesState.filter((article) => article.id !== payload.id)
+    case DELETE_ARTICLE: {
+      const newArticles = { ...articlesState }
+      delete newArticles[payload.id]
+      return newArticles
+    }
 
     default:
       return articlesState
