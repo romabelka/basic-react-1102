fc90b94c087437938b8b027aea39e4d9048eb96f
Alexander Gruzdkov <gruzdkow.a@ya.ru>
2019-02-27 20:49:15 +0300

    ht 4.2 is done

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from '../middlewares/logger'
+import idGenerator from '../middlewares/id-generator'
 import reducer from '../reducer'
 
-const enhancer = applyMiddleware(logger)
+const enhancer = applyMiddleware(logger, idGenerator)
 
 const store = createStore(reducer, enhancer)
 
